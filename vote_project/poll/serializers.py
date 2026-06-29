d26e40ec082a337e629719c621d44c1f2c67dc79
bmedmilbe <bm.edmilbe@gmail.com>
2026-06-29 12:50:20 +0100

    fix: restore user serializer

--- vote_project/poll/serializers.py
+++ vote_project/poll/serializers.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-from .models import (
+from poll.models import (
     AccumulatedResult,
     CandidateRegistration,
     Constituency,
@@ -12,6 +14,47 @@ from .models import (
     User,
     VoteCount,
 )
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError('Passwords must match.')
+        return data
+
+    def create(self, validated_data):
+        data = {
+            key: value for key, value in validated_data.items()
+            if key not in ('password1', 'password2')
+        }
+        data['password'] = validated_data['password1']
+        return self.Meta.model.objects.create_user(**data)
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            'id', 'username', 'password1', 'password2',
+            'first_name', 'last_name',
+        )
+        read_only_fields = ('id',)
+
+class SignInSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        for field in user._meta.fields:
+            field_name = field.name
+            if field_name not in ['password','id']:
+                token[field_name] = str(getattr(user, field_name))
+        
+        return token
+
+
+
 
 # ==========================================
 # USER SERIALIZER
@@ -255,3 +298,4 @@ class CandidateRankingSerializer(serializers.Serializer):
     total_votes = serializers.IntegerField()
     percentage = serializers.FloatField()
     estimated_seats = serializers.IntegerField(required=False)
+        
